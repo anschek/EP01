9cba48339de2ff4b6f62787c0e22d6188c58ddcd
anschek <anschek@yandex.ru>
2025-03-06 10:51:02 +0300

    test: add 5 tests for CheckMark

--- REG_MARK_LIB.Test/RegMarkService_Test.cs
+++ REG_MARK_LIB.Test/RegMarkService_Test.cs
@@ -2,6 +2,56 @@ namespace REG_MARK_LIB.Test
 {
     [TestClass]
     public class RegMarkService_Test
-    {
+    {   
+        [TestClass] 
+        public class PositiveTests
+        {
+            [TestMethod]
+            public void CheckMark_CorrectMarkLowerCase_ResultEqualExpected()
+            {
+                string mark = "a001aa52";
+                bool expected = true;
+
+                bool result = RegMarkService.CheckMark(mark);
+                Assert.AreEqual(expected, result);
+            }
+            [TestMethod]
+            public void CheckMark_CorrectMarkUpperCase_ResultIsTrue()
+            {
+                string mark = "B122BB152";
+
+                bool result = RegMarkService.CheckMark(mark);
+                Assert.IsTrue(result);
+            }
+            [TestMethod]
+            public void CheckMark_WrongRegion_ResultNotEqualNotExpected()
+            {
+                string mark = "a001aa100";
+                bool notExpected = true;
+
+                bool result = RegMarkService.CheckMark(mark);
+                Assert.AreNotEqual(notExpected, result);
+            }
+            [TestMethod]
+            public void CheckMark_WrongRegNum_ResultIsFalse()
+            {
+                string mark = "a0a0aa152";
+
+                bool result = RegMarkService.CheckMark(mark);
+                Assert.IsFalse(result);
+            }
+        }
+        [TestClass]
+        public class NegativeTests
+        {
+            [TestMethod]
+            public void CheckMark_Cyrillic—haractersSeries_ResultIsFalse()
+            {
+                string mark = "Ù001aa10";
+
+                bool result = RegMarkService.CheckMark(mark);
+                Assert.IsFalse(result);
+            }
+        }
     }
 }
