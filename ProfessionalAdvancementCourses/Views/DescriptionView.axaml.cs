2d78fc709bd91f9f0173b3e13d88eeba3af4d24e
anschek <anschek@yandex.ru>
2025-03-17 16:03:50 +0300

    layout: add styles

---
+++ ProfessionalAdvancementCourses/Views/DescriptionView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ProfessionalAdvancementCourses.Views;
+
+public partial class DescriptionView : UserControl
+{
+    public DescriptionView()
+    {
+        InitializeComponent();
+    }
+}
